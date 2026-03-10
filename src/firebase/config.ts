d1ef5da30274a59d068ce6a5eaeca8d43c43a941
זוהר דוידוב <davidov.zohar@gmail.com>
2026-03-10 18:05:24 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2124938424-e8a45",
+  "appId": "1:488713385252:web:1637a14f285aaadd5c2fb1",
+  "apiKey": "AIzaSyD3-hZV9btjR46Fyx3UsAehGrBi8BEZPBo",
+  "authDomain": "studio-2124938424-e8a45.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "488713385252"
+};
